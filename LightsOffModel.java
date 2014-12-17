87ab0f8a3a09cdaa4601e12d0e0e175c8e30c8c7
Gabriel Frassl <gabrielfrassl98@gmail.com>
2014-12-17 11:53:22 +0100

    neue und kommentierte version des Models /17.12.14

--- LightsOffModel.java
+++ LightsOffModel.java
@@ -1,13 +1,28 @@
 package frassl;
-
+/**
+ * die Logik klasse für das Spiel Lightsoff
+ * version 17.12.2014
+ * @author Gabriel
+ * attribut MArray: das logikarray welches später in 
+ * der View gezeichnet werden wird
+ *
+ */
 public class LightsOffModel {
 	private int[][] MArray = new int[5][5];
 	
+	/**
+	 * Construktor des Models der die Methode newGame() ausführt
+ 	 * @author Gabriel Frassl
+	 */
 	public LightsOffModel(){
 		newGame();
 		
 	}
-	
+	/**
+	 * die Methode die ein neues LogikArray generiert
+	 * Sie wird beim start des Spieles aufgerufen
+	 * @author Gabriel
+	 */
 	public void newGame(){
 		for(int i = 0; i<5; i++){
 			
@@ -17,12 +32,22 @@ public class LightsOffModel {
 		}
 	}
 }
+	/**
+	 * Wenn ein Button gedrückt wird wird in dieser Methode 
+	 * das Logikarray geändert,welches in der View wieder gezeichnet wird
+	 * @author Gabriel Frassl
+	 * @param x Die x koordinate des gedrückten Buttons
+	 * @param y Die y koordinate des gedrückten Buttons
+	 */
 	public void changeGame(int x,int y){
+		//Ändern des Wertes des gedrückten buttons
 		if(MArray[x][y]== 0){
 			MArray[x][y]=1;
 			
 		}else{ MArray[x][y]=0;}
 		
+		// Ändern der Werte der Buttons Links und rechts des 
+		// gedrückten buttons...
 		
 		int Xmin= x-1; 
 		int Xplus= x+1;
@@ -53,7 +78,8 @@ public class LightsOffModel {
 			
 		}
 		
-		// yyyyyyyyyyyy
+		// Ändern der Werte der Buttons Oberhalb und unterhalb des 
+			// gedrückten buttons...
 		int Ymin= x-1; 
 		int Yplus= x+1;
 		if(x > 0 && x <5){
@@ -85,6 +111,13 @@ public class LightsOffModel {
 		
 	}
 
+	/**
+	 * die getmethode für das Logikarray
+	 * Wird im Controller der View übergeben, welche
+	 * das Array benötigt um die Graphik zu erstellen
+	 * @author Gabriel Frassl
+	 * @return Das Logikarray MArray
+	 */
 public int[][] getArray(){
 	return this.MArray;
 }
